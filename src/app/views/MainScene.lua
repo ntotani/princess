8cd0a0cddb894bc9b9051a69813dbcde5fc988ad
Naoyuki Totani <nyk.totani@gmail.com>
2015-07-15 05:15:20 +0900

    移動先に応じて動く

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -16,6 +16,18 @@ local CHIPS = {
     flf  = {{i = -2, j =  0}, {i = -1, j = -1}},
 }
 
+local TILES = {
+    {0, 0, 1, 0, 0},
+    {0, 1, 0, 1, 0},
+    {1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0},
+    {1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0},
+    {1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0},
+    {0, 0, 1, 0, 0},
+}
+
 function MainScene:onCreate()
     --[[
     local TILES_PER_SIDE = 3
@@ -23,25 +35,15 @@ function MainScene:onCreate()
         return us.rep(0, TILES_PER_SIDE * 2 - 1)
     end)
     ]]
-    local tiles = {
-        {0, 0, 1, 0, 0},
-        {0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0},
-        {0, 0, 1, 0, 0},
-    }
     cc.TMXTiledMap:create("tmx/forest.tmx"):addTo(self)
-    for i, line in ipairs(tiles) do
+    for i, line in ipairs(TILES) do
         for j, e in ipairs(line) do
             if e == 1 then
                 display.newSprite("img/tile.png"):move(self:idx2pt(i, j)):addTo(self)
             end
         end
     end
+    self.enemies = display.newLayer():addTo(self)
     self.friends = display.newLayer():addTo(self)
     self:initChara(9, 3, "hime", true)
     self:initChara(8, 2, "witch", true)
@@ -63,11 +65,11 @@ function MainScene:initChara(i, j, job, isFriend)
     local node = cc.Node:create():move(self:idx2pt(i, j))
     node.sprite = jam.sprite("img/" .. job .. ".png", 32):addTo(node)
     if isFriend then
-        node.sprite:frameIdx(9, 10, 11)
+        node.sprite:frameIdx(9, 10, 11, 10)
         node:addTo(self.friends)
     else
-        node.sprite:frameIdx(0, 1, 2)
-        node:addTo(self)
+        node.sprite:frameIdx(0, 1, 2, 1)
+        node:addTo(self.enemies)
     end
     node.idx = {i = i, j = j}
 end
@@ -96,11 +98,27 @@ function MainScene:onTouch(e)
             local x, y = friend:getPosition()
             if cc.rectContainsPoint(cc.rect(x - len / 2, y - len / 2, len, len), e) then
                 for _, dir in ipairs(CHIPS[self.holdChip.name]) do
-                    friend.idx.i = friend.idx.i + dir.i
-                    friend.idx.j = friend.idx.j + dir.j
-                    friend:move(self:idx2pt(friend.idx.i, friend.idx.j))
+                    local ni = friend.idx.i + dir.i
+                    local nj = friend.idx.j + dir.j
+                    if ni < 1 or ni > #TILES or nj < 1 or nj > #TILES[1] or TILES[ni][nj] == 0 then
+                        -- out of bounds
+                        friend:removeSelf()
+                        break
+                    end
+                    local charas = us.flatten({self.friends:getChildren(), self.enemies:getChildren()})
+                    local hit = us.detect(charas, function(e)
+                        return us.isEqual(e.idx, {i = ni, j = nj})
+                    end)
+                    friend.idx.i = ni
+                    friend.idx.j = nj
+                    friend:move(self:idx2pt(ni, nj))
+                    if hit then
+                        -- kill other chara
+                        charas[hit]:removeSelf()
+                        break
+                    end
                 end
-                break
+                break -- TODO draw chip here
             end
         end
         self.holdChip:move(self.holdChip.backPt)
