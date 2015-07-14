69b51e3dd414d440b2e1e4199dc0865c75b5f9d4
Naoyuki Totani <nyk.totani@gmail.com>
2015-07-14 21:53:17 +0900

    チップを表示する

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -2,6 +2,20 @@ local us = require("lib.moses")
 local jam = require("lib.jam")
 local MainScene = class("MainScene", cc.load("mvc").ViewBase)
 
+local CHIPS = {
+    f    = {{i = -2, j =  0}},
+    rf   = {{i = -1, j =  1}},
+    lf   = {{i = -1, j = -1}},
+    rb   = {{i =  1, j =  1}},
+    lb   = {{i =  1, j = -1}},
+    b    = {{i =  2, j =  0}},
+    ff   = {{i = -2, j =  0}, {i = -2, j =  0}},
+    rfrf = {{i = -1, j =  1}, {i = -1, j =  1}},
+    lflf = {{i = -1, j = -1}, {i = -1, j = -1}},
+    frf  = {{i = -2, j =  0}, {i = -1, j =  1}},
+    flf  = {{i = -2, j =  0}, {i = -1, j = -1}},
+}
+
 function MainScene:onCreate()
     --[[
     local TILES_PER_SIDE = 3
@@ -34,6 +48,11 @@ function MainScene:onCreate()
     jam.sprite("img/hime.png", 32):frameIdx(0, 1, 2):addTo(cc.Node:create():move(self:idx2pt(1, 3)):addTo(self))
     jam.sprite("img/witch.png", 32):frameIdx(0, 1, 2):addTo(cc.Node:create():move(self:idx2pt(2, 4)):addTo(self))
     jam.sprite("img/ninja.png", 32):frameIdx(0, 1, 2):addTo(cc.Node:create():move(self:idx2pt(3, 1)):addTo(self))
+    local names = us.keys(CHIPS)
+    for i = 1, 4 do
+        local chip = names[math.random(1, #names)]
+        display.newSprite("chip/" .. chip .. ".png"):move((72 + 14) * (i - 1) + 15 + 36, 80):addTo(self)
+    end
 end
 
 function MainScene:idx2pt(i, j)
