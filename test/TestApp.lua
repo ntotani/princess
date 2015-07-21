4f3d6216e7c7068833b9b6feb3e94280baac35af
Naoyuki Totani <nyk.totani@gmail.com>
2015-07-22 05:06:22 +0900

    ゲームシーンのテストをリネーム

--- test/TestApp.lua
+++ test/TestApp.lua
@@ -1,11 +1,3 @@
-
-cc.FileUtils:getInstance():setPopupNotify(false)
-cc.FileUtils:getInstance():addSearchPath("src/")
-cc.FileUtils:getInstance():addSearchPath("res/")
-
-require "config"
-require "cocos.init"
-
 local TestApp = class("TestApp", cc.load("mvc").AppBase)
 
 function TestApp:onCreate()
@@ -27,12 +19,5 @@ function TestApp:commit(charaId, chipIdx)
     self.listener({charaId .. chipIdx})
 end
 
-local function main()
-    TestApp:create():run("GameScene")
-end
-
-local status, msg = xpcall(main, __G__TRACKBACK__)
-if not status then
-    print(msg)
-end
+return TestApp
 
