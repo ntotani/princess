be0fa6592525a96635df057327aa6944bad59b02
Naoyuki Totani <nyk.totani@gmail.com>
2015-07-23 18:45:52 +0900

    ユニットテスト

---
+++ test/main.lua
@@ -0,0 +1,26 @@
+local luaunit = require("test.lib.luaunit")
+
+function testSample()
+    local scene = cc.Scene:create()
+    display.runScene(scene)
+    cc.Director:getInstance():mainLoop() -- run scene
+
+    local node = cc.Node:create()
+    scene:addChild(node)
+    node:runAction(cc.MoveTo:create(5, cc.p(10, 0)))
+    cc.Director:getInstance():mainLoop() -- first tick
+
+    setDeltaTime(5)
+    cc.Director:getInstance():mainLoop() -- animation
+    luaunit.assertEquals(node:getPositionX(), 10)
+end
+
+cc.FileUtils:getInstance():setPopupNotify(false)
+cc.FileUtils:getInstance():addSearchPath("src/")
+cc.FileUtils:getInstance():addSearchPath("res/")
+
+require "config"
+require "cocos.init"
+
+os.exit(luaunit.LuaUnit.run('-v'))
+
