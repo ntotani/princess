ea315c2c2a38f19c5868d85801299f6368b97cc0
Naoyuki Totani <nyk.totani@gmail.com>
2015-07-25 18:02:06 +0900

    Scene作るテスト準備

--- test/TestApp.lua
+++ test/TestApp.lua
@@ -2,7 +2,7 @@ local TestApp = class("TestApp", cc.load("mvc").AppBase)
 
 function TestApp:onCreate()
     self.shogi = require("lib.shogi").new({random = function()
-        return math.random(4294967295)
+        return math.random(65535)
     end})
 end
 
--- test/cases/TestGameScene.lua
+++ test/cases/TestGameScene.lua
@@ -3,7 +3,15 @@ local us = require("src.lib.moses")
 
 TestGameScene = {}
 
-function TestGameScene:testSample()
-    luaunit.assertEquals(1 + 2, 3)
+function TestGameScene:setUp()
+    local app = require("test.TestApp"):create()
+    app.shogi:commitForm({red = {"193"}, blue = {"113"}})
+    self.scene = require("app.views.GameScene"):create(app, "GameScene")
+    self.scene:showWithScene()
+    cc.Director:getInstance():mainLoop() -- run scene
+end
+
+function TestGameScene:testReset()
+    luaunit.assertEquals(#self.scene:getChildren(), 25)
 end
 
