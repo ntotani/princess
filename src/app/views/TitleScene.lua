6ac0a895d750b941471823c69dfeb6bfe87b37f8
Naoyuki Totani <nyk.totani@gmail.com>
2015-08-04 15:26:12 +0900

    部屋番号バックスペース

--- src/app/views/TitleScene.lua
+++ src/app/views/TitleScene.lua
@@ -30,10 +30,28 @@ end
 function TitleScene:onJoin()
     self.titles:moveBy({time = 0.2, x = -display.width})
     self.smoke:fadeTo({time = 0.2, opacity = 127, onComplete = function()
-        local message = cc.Label:createWithTTF("部屋番号を入力して下さい", "font/PixelMplus12-Regular.ttf", 24):move(display.cx, display.height * 0.8):addTo(self)
+        local EMPTY_MESSAGE = "部屋番号を入力して下さい"
+        local message = cc.Label:createWithTTF(EMPTY_MESSAGE, "font/PixelMplus12-Regular.ttf", 24):move(display.cx, display.height * 0.8):addTo(self)
         local menu = cc.Menu:create():move(0, 0):addTo(self)
         local nums = cc.Node:create():addTo(self)
         local roomId = ""
+        local commit = cc.MenuItemImage:create("img/button_ok.png", "img/button_ok.png"):move(display.cx, 72):hide():addTo(menu):onClicked(function()
+            if roomId == "" then return end
+            menu:removeSelf()
+            nums:removeSelf()
+            self:getApp():joinRoom(roomId)
+        end)
+        local backspace = nil
+        backspace = cc.MenuItemImage:create("img/button_bs.png", "img/button_bs.png"):move(display.width * 0.8, display.height * 0.8):hide():addTo(menu):onClicked(function()
+            roomId = string.sub(roomId, 1, -2)
+            if roomId == "" then
+                message:setString(EMPTY_MESSAGE)
+                commit:hide()
+                backspace:hide()
+            else
+                message:setString(roomId)
+            end
+        end)
         for i = 0, 9 do
             local x = display.cx + (i % 3 - 1) * 80
             local y = display.cy + (1 - math.floor(i / 3)) * 80
@@ -45,15 +63,11 @@ function TitleScene:onJoin()
             cc.MenuItemImage:create("img/btn.png", "img/btn.png"):move(x, y):addTo(menu):onClicked(function()
                 roomId = roomId .. num
                 message:setString(roomId)
+                commit:show()
+                backspace:show()
             end)
             cc.Label:createWithTTF(num, "font/PixelMplus12-Regular.ttf", 24):move(x, y):addTo(nums):setColor(display.COLOR_BLACK)
         end
-        cc.MenuItemImage:create("img/button_ok.png", "img/button_ok.png"):move(display.cx, 72):addTo(menu):onClicked(function()
-            if roomId == "" then return end
-            menu:removeSelf()
-            nums:removeSelf()
-            self:getApp():joinRoom(roomId)
-        end)
     end})
 end
 
